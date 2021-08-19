a579afbdd062708ed389ee4a58f5dcd7695fd664
ArturoCarrilloSolorzano <arturo.carrillo.solorzano@gmail.com>
2021-08-19 00:53:08 -0500

    update

--- src/view/index.js
+++ src/view/index.js
@@ -2,7 +2,12 @@ import './styles.css';
 import Phone from '../svg/phone';
 import Twitter from '../svg/twitter';
 import Mail from '../svg/mail';
+import Tailwind from '../svg/tailwind';
 import ProgressBar from '../components/progressBar';
+import laravel from '../technologies/laravel.png';
+import nodejs from '../technologies/nodejs.png';
+import reactImg from '../technologies/react.png';
+import nextImg from '../technologies/next.png';
 
 export default function Curriculum() {
     return (
@@ -12,7 +17,7 @@ export default function Curriculum() {
               <div className="grid grid-cols-4">
                 <div className="bg-indigo-900 h-full p-2">
                     <div className="flex justify-center">
-                        <div class="bg-gray-200 rounded-full h-56 w-56 flex items-center justify-center mt-10"></div>
+                        <div className="bg-gray-200 rounded-full h-56 w-56 flex items-center justify-center mt-10"></div>
                     </div>
                     <div className="mt-10">
                         <div className="text-3xl text-white">Arturo Carrillo Solorzano</div>
@@ -72,15 +77,32 @@ export default function Curriculum() {
                             }}/>
                         </div>
                         <div className="mt-5 text-lg text-left">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                        I am currently a student at the University of Guadalajara in Computer Engineering
+                        im also working as a freelancer i have worked on several web development and app proyects.
+                        I have experience on backend, frontend and devops. I really like working on JS based technologies such as React, React Native and Nodejs.
+                        </div>
+                        <div className="text-2xl mt-10">Technologies</div>
+                        <div className="flex justify-center p-5">
+                            <div>
+                                <img src={laravel} width={100} className="p-3" />
+                            </div>
+                            <div>
+                                <img src={nodejs} width={100}  className="p-3" />
+                            </div>
+                            <div>
+                                <Tailwind />
+                            </div>
+                            <div>
+                                <img src={reactImg} width={100}  className="p-3" />
+                            </div>
+                            <div>
+                                <img src={nextImg} width={100}  className="p-3" />
+                            </div>
                         </div>
                     </div>
 
                     <div>
-                        <div className="mt-20"/>
+                        <div className="mt-10"/>
                         <div className="text-4xl">Work Experience</div>
                         <div className="mt-2">
                             <hr  style={{
@@ -91,10 +113,17 @@ export default function Curriculum() {
                             }}/>
                         </div>
                         <div className="mt-5 text-lg text-left">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                            <div className="grid grid-cols-4">
+                                <div>
+                                    Freelancer
+                                </div>
+                                <div className="col-span-3">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                </div>
+                            </div>
                         </div>
                     </div>
 
@@ -110,10 +139,26 @@ export default function Curriculum() {
                             }}/>
                         </div>
                         <div className="mt-5 text-lg text-left">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                            <div className="grid grid-cols-4">
+                                <div className="mt-5">
+                                    University of Guadalajara
+                                </div>
+                                <div className="col-span-3 mt-5">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                </div>
+                                <div className="mt-5">
+                                    Industrial technical teaching center (CETI)
+                                </div>
+                                <div className="col-span-3 mt-5">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                </div>
+                            </div>
                         </div>
                     </div>
 
